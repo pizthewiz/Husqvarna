744aecce2a3cc148927e3fe07e7d86b7a92f122e
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-08-15 20:51:50 -0700

    provide some basic arg parsing and setup

--- Husqvarna/main.m
+++ Husqvarna/main.m
@@ -7,11 +7,48 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "NSURL+CCAdditions.h"
+
+void dumpUsage(void);
+
+void dumpUsage(void) {
+    NSLog(@"Husqvarna SOURCE_FILE OUTPUT_FILE SETTINGS [...]");
+}
 
 int main (int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        if (argc < 4) {
+            dumpUsage();
+            exit(1);
+        }
+
+        NSString* sourceFileLocation = [NSString stringWithUTF8String:argv[1]];
+        NSURL* sourceFileURL = [NSURL fileURLWithString:sourceFileLocation];
+//        NSLog(@"sourceFileURL: %@", sourceFileURL);
+
+        NSError* error;
+        if (![sourceFileURL checkResourceIsReachableAndReturnError:&error]) {
+            NSLog(@"ERROR - bad source image url: %@", [error localizedDescription]);
+            exit(1);
+        }
+
+        NSString* outputFileLocation = [NSString stringWithUTF8String:argv[2]];
+        NSURL* outputFileURL = [NSURL fileURLWithString:outputFileLocation];
+        NSLog(@"outputFileURL: %@", outputFileURL);
+        if ([outputFileURL checkResourceIsReachableAndReturnError:NULL]) {
+            NSLog(@"will overwrite: %@", outputFileURL);
+        }
+
+        NSString* outputSettings = [NSString stringWithUTF8String:argv[3]];
+        NSLog(@"outputSettings: %@", outputSettings);
+        NSArray* components = [[outputSettings lowercaseString] componentsSeparatedByString:@"x"];
+        if (components.count != 2) {
+            NSLog(@"ERROR - bad output size provided '%@' should be WxH", outputSettings);
+        }
+        NSUInteger outputWidth = [[components objectAtIndex:0] integerValue];
+        NSUInteger outputHeight = [[components objectAtIndex:1] integerValue];
+
+        NSLog(@"width: %lu, height: %lu", outputWidth, outputHeight);
     }
     return 0;
 }
