5cc00a2a416c6468850e834277c045134520a30d
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-08-15 20:18:07 -0700

    initial import

---
+++ Husqvarna/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Husqvarna
+//
+//  Created by Jean-Pierre Mouilleseaux on 15 Aug 2011.
+//  Copyright (c) 2011 Chorded Constructions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
