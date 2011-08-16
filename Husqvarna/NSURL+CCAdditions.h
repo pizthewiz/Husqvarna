53011cdecd4b2e93299fa88a58f5aed5f2ea767c
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-08-15 20:19:39 -0700

    add placeholder for NSURL category

---
+++ Husqvarna/NSURL+CCAdditions.h
@@ -0,0 +1,12 @@
+//
+//  NSURL+CCAdditions.h
+//  Husqvarna
+//
+//  Created by Jean-Pierre Mouilleseaux on 15 Aug 2011.
+//  Copyright (c) 2011 Chorded Constructions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSURL(CCAdditions)
+@end
---
+++ Husqvarna/NSURL+CCAdditions.m
@@ -0,0 +1,12 @@
+//
+//  NSURL+CCAdditions.m
+//  Husqvarna
+//
+//  Created by Jean-Pierre Mouilleseaux on 15 Aug 2011.
+//  Copyright (c) 2011 Chorded Constructions. All rights reserved.
+//
+
+#import "NSURL+CCAdditions.h"
+
+@implementation NSURL(CCAdditions)
+@end
